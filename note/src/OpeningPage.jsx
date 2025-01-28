4f4ba78f61724ea100c91925076b6a88d47077ab
Doğukan SOLAK <ahmetdogukansolak@gmail.com>
2025-01-28 03:42:51 +0300

    Not iki sayfa
    2 sayfa front bitti

--- note/src/OpeningPage.jsx
+++ note/src/OpeningPage.jsx
@@ -1,7 +1,8 @@
 // src/OpeningPage.jsx
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import { useEffect } from 'react';
+import "./OpeningPage.css";
 
 const Home = () => {
     const navigate = useNavigate();  
@@ -9,12 +10,17 @@ const Home = () => {
     const handleClick = () => {
       navigate('/Directing');  
     };
-  
+    useEffect(() => {
+      document.body.style.backgroundColor = '#5a649d'; 
+  }, []);
     return (
-      <div>
-        <h2>Note, Calendar, and More</h2>
+      <div className="yerlestirme texts">
+        <div className='logo'>
+        <img src="/public/logonotizen.PNG" alt="" />
+      </div>
+        <h1>Note, Calendar, and More</h1>
         <p>Choose what's good for you</p>
-        <button onClick={handleClick}>Click to start</button>
+        <button style={{color:'#5a649d', backgroundColor:'white', border:'1px solid #5a649d'}} onClick={handleClick}>Click to start</button>
       </div>
     );
   };
